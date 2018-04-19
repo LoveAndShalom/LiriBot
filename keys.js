d57c02a73a9207da79195f8c4901ed93813527b6
Twidt <t.c.twidt@gmail.com>
2018-04-19 00:15:48 -0400

    updating acess tokens

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "rTuiLuwNjhfNRe1jOwOZgQVk2",
+  consumer_secret: "JxkGVJeSDThTdKFAl7SWL9sL1jZ4eKi8vGHLe3BeJe6UJgsXz3",
+  access_token_key: "4534161674-LB9xg2HhKEHJ5QDdvRT3hvEWdPNCBEHULKYin0r",
+  access_token_secret: "COqNoaPupML7yfdkwTVVeDO4sfmDXRvhfpbY1ytau3dq9"
+};
+
+exports.spotifyKeys = {
+ client_id: "5040d7d1748a4d7386ca5af5e9efa597",
+  client_secret: "4b7528a5c10e4877b99ec7673d52f4bf"
+};
